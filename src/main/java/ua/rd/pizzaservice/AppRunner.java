3dd4b03f14fa9f6ad1a2f32ad93c219e741e00e7
Anton Mishkurov <Anton_Mishkurov@epam.com>
2016-10-04 12:08:25 +0300

    initial commit

---
+++ src/main/java/ua/rd/pizzaservice/AppRunner.java
@@ -0,0 +1,10 @@
+package ua.rd.pizzaservice;
+
+/**
+ * @author Anton_Mishkurov
+ */
+public class AppRunner {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/ua/rd/pizzaservice/AppRunnerTest.java
@@ -0,0 +1,10 @@
+package ua.rd.pizzaservice;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Anton_Mishkurov
+ */
+public class AppRunnerTest {
+
+}
