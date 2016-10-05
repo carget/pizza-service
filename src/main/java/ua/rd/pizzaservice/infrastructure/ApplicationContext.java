9135587a40b6834b487e1f36e851c3e3ed5b2c22
Anton Mishkurov <Anton_Mishkurov@epam.com>
2016-10-05 12:38:05 +0300

    Add BeanBuilder class

--- src/main/java/ua/rd/pizzaservice/infrastructure/ApplicationContext.java
+++ src/main/java/ua/rd/pizzaservice/infrastructure/ApplicationContext.java
@@ -18,35 +18,68 @@ public class ApplicationContext implements Context {
 
     @Override
     public <T> T getBean(String name) {
-        if (beans.containsKey(name)) {
-            return (T) beans.get(name);
-        }
         Class<?> type = config.getImpl(name);
-        Constructor<?> constructor = type.getConstructors()[0];
-        T bean = getBeanFromConstructor(constructor);
-        beans.put(name, bean);
-        return bean;
-    }
+        Object bean = beans.get(name);
 
-    private <T> T getBeanFromConstructor(Constructor<?> constructor) {
-        try {
-            int parameterCount = constructor.getParameterCount();
-            Object[] params = new Object[parameterCount];
-            Class<?>[] parameterTypes = constructor.getParameterTypes();
-            for (int i = 0; i < parameterCount; i++) {
-                String beanName = covertTypeToBeanName(parameterTypes[i].getSimpleName());
-                params[i] = getBean(beanName);
-            }
-            return (T) constructor.newInstance(params);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        if (bean != null) {
+            return (T) bean;
         }
+
+        BeanBuilder builder = new BeanBuilder(type);
+        builder.createBean();
+        //configuration
+//      builder.createBeanProxy();
+//      builder.callPostCreateMethod();
+//      builder.callInitMethod();
+        bean = builder.build();
+
+        beans.put(name, bean);
+
+        return (T) bean;
+
     }
 
-    private String covertTypeToBeanName(String name) {
-        char[] chars = name.toCharArray();
-        chars[0] = Character.toLowerCase(chars[0]);
-        return new String(chars);
+    private class BeanBuilder {
+        private Class<?> type;
+        private Object bean;
+
+        public BeanBuilder(Class<?> type) {
+            this.type = type;
+        }
+
+        private <T> T build() {
+            return (T) bean;
+        }
+
+        private <T> T createBean() {
+            Constructor<?> constructor = type.getConstructors()[0];
+            bean = getBeanFromConstructor(constructor);
+            return (T) bean;
+        }
+
+        private <T> T getBeanFromConstructor(Constructor<?> constructor) {
+            try {
+                int parameterCount = constructor.getParameterCount();
+                Object[] params = new Object[parameterCount];
+                Class<?>[] parameterTypes = constructor.getParameterTypes();
+                for (int i = 0; i < parameterCount; i++) {
+                    String beanName = covertTypeToBeanName(parameterTypes[i].getSimpleName());
+                    params[i] = getBean(beanName);
+                }
+                return (T) constructor.newInstance(params);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        private String covertTypeToBeanName(String name) {
+            char[] chars = name.toCharArray();
+            chars[0] = Character.toLowerCase(chars[0]);
+            return new String(chars);
+        }
+
     }
+
 
 }
